bfe01e834b0d6e95192fbeb05d2c23950e6d8204
shrek301 <tim_lat@mail.ru>
2018-11-30 05:51:16 +0000

    new update
    schema for test

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,25 +1,63 @@
-CREATE TABLE IF NOT EXISTS Person (
-    id         INTEGER  PRIMARY KEY AUTO_INCREMENT,
-    version    INTEGER NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    age        INTEGER  NOT NULL
-);
+CREATE TABLE IF NOT EXISTS Organization (
+      id   INTEGER  PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(50) NOT NULL,
+      fullname VARCHAR(50) UNIQUE NOT NULL,
+      inn INTEGER UNIQUE NOT NULL,
+      kpp INTEGER NOT NULL,
+      address VARCHAR(50) NOT NULL,
+      phone VARCHAR(50) ,
+      isActive  BOOLEAN
+  );
+  CREATE TABLE IF NOT EXISTS Office (
+      id   INTEGER  PRIMARY KEY AUTO_INCREMENT,
+      orgid  INTEGER NOT NULL,
+      name VARCHAR(50) NOT NULL,
+      address VARCHAR(50) NOT NULL,
+      phone VARCHAR(50) ,
+      isActive  BOOLEAN
+  );
 
-CREATE TABLE IF NOT EXISTS House (
-    id         INTEGER  PRIMARY KEY AUTO_INCREMENT,
-    version    INTEGER NOT NULL,
-    address    VARCHAR(50) NOT NULL
-);
+  CREATE TABLE IF NOT EXISTS User (
+      id   INTEGER  PRIMARY KEY AUTO_INCREMENT,
+      officeid  INTEGER NOT NULL,
+      firstname VARCHAR(50) NOT NULL,
+      secondname VARCHAR(50) ,
+      middlename VARCHAR(50) ,
+      position VARCHAR(50) NOT NULL,
+      phone VARCHAR(50) ,
+      docName VARCHAR(50) ,
+      docNumber INTEGER ,
+      docDate TIMESTAMP,
+      citizenshipName VARCHAR(50) ,
+      citizenshipCode INTEGER ,
+      isIdentified BOOLEAN ,
+      isActive  BOOLEAN
+  );
+  CREATE TABLE IF NOT EXISTS dosc (
+      id      INTEGER  PRIMARY KEY AUTO_INCREMENT,
+      name    VARCHAR(50) ,
+      code    INTEGER
+ );
 
-CREATE TABLE IF NOT EXISTS Person_House (
-    person_id   INTEGER  NOT NULL,
-    house_id    INTEGER  NOT NULL,
+  CREATE TABLE IF NOT EXISTS countries (
+      id      INTEGER  PRIMARY KEY AUTO_INCREMENT,
+      name    VARCHAR(50) ,
+      code    INTEGER
+ );
 
-    PRIMARY KEY (person_id, house_id)
-);
+   CREATE TABLE IF NOT EXISTS User_Organization_Office (
+      user_id   INTEGER  NOT NULL,
+      org_id    INTEGER  NOT NULL,
+      office_id INTEGER  NOT NULL,
 
-CREATE INDEX IX_Person_House_Id ON Person_House (house_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (house_id) REFERENCES House(id);
+      PRIMARY KEY (person_id, org_id, office_id)
+  );
 
-CREATE INDEX IX_House_Person_Id ON Person_House (person_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (person_id) REFERENCES Person(id);
+  CREATE INDEX IX_User_Organization_Office_Id ON User_Organization_Office (office_id);
+  ALTER TABLE User_Organization_Office ADD FOREIGN KEY (office_id) REFERENCES Office(id);
+
+  CREATE INDEX IX_Office_User_Organization_Id ON User_Organization_Office (user_id);
+  ALTER TABLE User_Organization_Office ADD FOREIGN KEY (user_id) REFERENCES User(id);
+
+  CREATE INDEX IX_Organization_User_Office_Id ON User_Organization_Office (org_id);
+  ALTER TABLE User_Organization_Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
