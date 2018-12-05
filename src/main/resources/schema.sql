d37dc9b4786935460f9ef06cdd8690b635183db6
shrek301 <tim_lat@mail.ru>
2018-12-05 11:52:43 +0000

    new
    new

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,92 +1,64 @@
 CREATE TABLE IF NOT EXISTS Organization
-  (
-    id        INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name      VARCHAR(50)        NOT NULL,
-    full_name VARCHAR(50) UNIQUE NOT NULL,
-    inn       VARCHAR(50) UNIQUE NOT NULL,
-    kpp       VARCHAR(50)        NOT NULL,
-    address   VARCHAR(50)        NOT NULL,
-    phone     VARCHAR(50),
-    is_active BOOLEAN
-  );
-  CREATE TABLE IF NOT EXISTS Office
-  (
-    id        INTEGER PRIMARY KEY AUTO_INCREMENT,
-    org_id    INTEGER     NOT NULL,
-    name      VARCHAR(50) NOT NULL,
-    address   VARCHAR(50) NOT NULL,
-    phone     VARCHAR(50),
-    is_active BOOLEAN
-  );
+(
+  id        INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  name      VARCHAR(50)                      NOT NULL COMMENT 'Имя',
+  full_name VARCHAR(50) COMMENT 'Имя' UNIQUE NOT NULL,
+  inn       VARCHAR(10) COMMENT 'ИНН' UNIQUE NOT NULL,
+  kpp       VARCHAR(9)                       NOT NULL COMMENT 'КПП',
+  address   VARCHAR(50)                      NOT NULL COMMENT 'Адрес',
+  phone     VARCHAR(50) COMMENT 'Телефон',
+  is_active BOOLEAN COMMENT 'Статус'
+);
+COMMENT ON TABLE Organization IS 'Организация';
+
+CREATE TABLE IF NOT EXISTS Office
+(
+  id        INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  org_id    INTEGER     NOT NULL COMMENT 'Уникальный идентификатор Организация',
+  name      VARCHAR(50) NOT NULL COMMENT 'Наименование',
+  address   VARCHAR(50) NOT NULL COMMENT 'Адрес',
+  phone     VARCHAR(50) COMMENT 'Телефон',
+  is_active BOOLEAN COMMENT 'Статус'
+);
+COMMENT ON TABLE Office IS 'Офис';
+
+CREATE TABLE IF NOT EXISTS User
+(
+  id            INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  first_name    VARCHAR(50) NOT NULL COMMENT 'Имя',
+  second_name   VARCHAR(50) COMMENT 'Фамилия',
+  middle_name   VARCHAR(50) COMMENT 'Отчество',
+  position      VARCHAR(50) NOT NULL COMMENT 'Позиция',
+  phone         VARCHAR(50) COMMENT 'Телефон',
+  is_identified BOOLEAN COMMENT 'Идентификация',
+  is_active     BOOLEAN COMMENT 'Статус'
+);
+COMMENT ON TABLE User IS 'Пользователь';
+
+CREATE TABLE IF NOT EXISTS Document_Type
+(
+  id       INTEGER COMMENT 'Возраст' PRIMARY KEY AUTO_INCREMENT,
+  doc_code VARCHAR(2)  NOT NULL COMMENT 'типа документа',
+  doc_name VARCHAR(50) NOT NULL COMMENT 'имя документа',
+);
+COMMENT ON TABLE Document_Type IS 'Тип документа';
 
 
-  CREATE TABLE IF NOT EXISTS User
-  (
-    id               INTEGER PRIMARY KEY AUTO_INCREMENT,
-    office_id        INTEGER     NOT NULL,
-    first_name       VARCHAR(50) NOT NULL,
-    second_name      VARCHAR(50),
-    middle_name      VARCHAR(50),
-    position         VARCHAR(50) NOT NULL,
-    phone            VARCHAR(50),
-    doc_name         VARCHAR(50),
-    doc_number       INTEGER,
-    doc_date         TIMESTAMP,
-    doc_code         INTEGER,
-    citizenship_name VARCHAR(50),
-    citizenship_code INTEGER,
-    is_identified    BOOLEAN,
-   is_active        BOOLEAN
-  );
+CREATE TABLE IF NOT EXISTS Document
+(
+  id         INTEGER COMMENT 'Возраст' PRIMARY KEY AUTO_INCREMENT,
+  doc_number INTEGER COMMENT 'номер документа',
+  doc_date   TIMESTAMP COMMENT 'дата документа',
+
+);
+COMMENT ON TABLE Document IS 'Документ';
+
+CREATE TABLE IF NOT EXISTS Citizenship
+(
+  id               INTEGER COMMENT 'Возраст' PRIMARY KEY AUTO_INCREMENT,
+  citizenship_name VARCHAR(50) COMMENT 'гражданство',
+  citizenship_code VARCHAR(3) COMMENT 'код гражданства'
+);
+COMMENT ON TABLE Citizenship IS 'Гражданство';
 
 
-  CREATE TABLE IF NOT EXISTS Document
- (
-    id   INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50),
-    code VARCHAR(50)
-  );
-
-
-  CREATE TABLE IF NOT EXISTS Citizenship
-  (
-    id   INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50),
-    code INTEGER
-  );
-
-
-  CREATE TABLE IF NOT EXISTS User_Document (
-  user_id   INTEGER  NOT NULL  ,
-  document_id    INTEGER  NOT NULL  ,
-
-
-  PRIMARY KEY (user_id, document_id)
-  );
-  CREATE TABLE IF NOT EXISTS User_Citizenship (
-  user_id   INTEGER  NOT NULL ,
-  citizenship_id    INTEGER  NOT NULL ,
-
-
-  PRIMARY KEY (user_id, citizenship_id)
-  );
-
-
-  CREATE INDEX IX_User_Document_Id ON User_Document (document_id);
-  ALTER TABLE User_Document
-    ADD FOREIGN KEY (document_id) REFERENCES Document (id);
-
-
-  CREATE INDEX IX_Document_User_Id ON User_Document (user_id);
- ALTER TABLE User_Document
-    ADD FOREIGN KEY (user_id) REFERENCES User (id);
-
-
-  CREATE INDEX IX_Citizenship_User_Id ON User_Citizenship ( citizenship_id );
-  ALTER TABLE User_Citizenship
-    ADD FOREIGN KEY ( citizenship_id ) REFERENCES Citizenship (id);
-
-
-  CREATE INDEX IX_User_Citizenship_Id ON User_Citizenship (user_id);
-  ALTER TABLE User_Citizenship
-    ADD FOREIGN KEY (user_id) REFERENCES User (id);
