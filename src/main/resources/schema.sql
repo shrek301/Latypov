4d679362a3b071ae78f31bb9d76e52987613bdf1
shrek301 <tim_lat@mail.ru>
2018-12-05 22:33:37 +0500

    new

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS Organization
 (
   id        INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-  user_id INTEGER , FOREIGN KEY (user_id) REFERENCES User(id),
-  office_id INTEGER , FOREIGN KEY (office_id) REFERENCES Office(id),
+  user_id INTEGER COMMENT 'Связь с таблицей Юзер',   FOREIGN KEY (user_id) REFERENCES User(id) ,
+  office_id INTEGER COMMENT 'Связь с таблицей Офис', FOREIGN KEY (office_id) REFERENCES Office(id),
   name      VARCHAR(50)                      NOT NULL COMMENT 'Имя',
   full_name VARCHAR(50) COMMENT 'Имя' UNIQUE NOT NULL,
   inn       VARCHAR(10) COMMENT 'ИНН' UNIQUE NOT NULL,
@@ -16,8 +16,8 @@ COMMENT ON TABLE Organization IS 'Организация';
 CREATE TABLE IF NOT EXISTS Office
 (
   id        INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-  user_id INTEGER , FOREIGN KEY (user_id) REFERENCES User(id),
-  organization_id INTEGER , FOREIGN KEY (organization_id) REFERENCES Organization(id),
+  user_id INTEGER COMMENT 'Связь с таблицей Юзер', FOREIGN KEY (user_id) REFERENCES User(id),
+  organization_id INTEGER COMMENT 'Связь с таблицей Организация', FOREIGN KEY (organization_id) REFERENCES Organization(id),
   name      VARCHAR(50) NOT NULL COMMENT 'Наименование',
   address   VARCHAR(50) NOT NULL COMMENT 'Адрес',
   phone     VARCHAR(50) COMMENT 'Телефон',
@@ -28,10 +28,10 @@ COMMENT ON TABLE Office IS 'Офис';
 CREATE TABLE IF NOT EXISTS User
 (
   id            INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-  doc_type_id   INTEGER, FOREIGN KEY (doc_type_id) REFERENCES Document_Type(id),
-  citizenship_id INTEGER , FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id),
-  office_id INTEGER , FOREIGN KEY (office_id) REFERENCES Office(id),
-  organization_id INTEGER , FOREIGN KEY (organization_id) REFERENCES Organization(id),
+  doc_type_id   INTEGER COMMENT 'Связь с таблицей Тип Документа', FOREIGN KEY (doc_type_id) REFERENCES Document_Type(id),
+  citizenship_id INTEGER COMMENT 'Связь с таблицей Гражданство', FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id),
+  office_id INTEGER COMMENT 'Связь с таблицей Офис', FOREIGN KEY (office_id) REFERENCES Office(id),
+  organization_id INTEGER COMMENT 'Связь с таблицей Организация', FOREIGN KEY (organization_id) REFERENCES Organization(id),
   first_name    VARCHAR(50) NOT NULL COMMENT 'Имя',
   second_name   VARCHAR(50) COMMENT 'Фамилия',
   middle_name   VARCHAR(50) COMMENT 'Отчество',
@@ -54,7 +54,7 @@ COMMENT ON TABLE Document_Type IS 'Тип документа';
 CREATE TABLE IF NOT EXISTS Document
 (
   id         INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-   doc_type_id   INTEGER, FOREIGN KEY (doc_type_id) REFERENCES Document_Type(id),
+   doc_type_id   INTEGER COMMENT 'Связь с таблицей Тип Документа', FOREIGN KEY (doc_type_id) REFERENCES Document_Type(id),
   doc_number INTEGER COMMENT 'номер документа',
   doc_date   TIMESTAMP COMMENT 'дата документа',
 
