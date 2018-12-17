f4f340a96b3750b4423a23d34c9317d1c937c776
shrek301 <tim_lat@mail.ru>
2018-12-17 11:58:24 +0000

    new code for OrganizationService

--- src/main/java/ru/latypov/service/OrganizationService.java
+++ src/main/java/ru/latypov/service/OrganizationService.java
@@ -1,4 +1,22 @@
 package ru.latypov.service;
 
-public class OrganizationService {
+import org.springframework.http.ResponseEntity;
+import ru.latypov.model.Organization;
+
+import java.util.List;
+
+/*Набор методов для контролера Организации */
+
+public interface OrganizationService {
+
+    public List<Organization> retrieveOrganization();
+
+    public Organization getOrganization(Integer id);
+
+    public void savesOrganization(Organization organization);
+
+    public ResponseEntity updateOrganization(Organization organization);
+
+    public Organization getOrganization(Organization organization);
 }
+
