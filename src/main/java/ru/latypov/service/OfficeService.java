35ac9c1514c3a04d9ac8bddbc5faa89efe11017e
shrek301 <tim_lat@mail.ru>
2018-12-17 11:57:09 +0000

    new code for  OfficeService

--- src/main/java/ru/latypov/service/OfficeService.java
+++ src/main/java/ru/latypov/service/OfficeService.java
@@ -1,4 +1,20 @@
 package ru.latypov.service;
 
-public class OfficeService {
+
+import org.springframework.http.ResponseEntity;
+import ru.latypov.model.Office;
+
+import java.util.List;
+
+/*Набор методов для контролера Офис */
+public interface OfficeService {
+    public List<Office> retrieveOffice();
+
+    public Office getOffice(Integer id);
+
+    public void savesOffice(Office office);
+
+    public ResponseEntity updateOffice(Office office);
+
+    public Office getOffice(Office office);
 }
