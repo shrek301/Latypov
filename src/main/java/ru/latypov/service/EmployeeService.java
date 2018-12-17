9cd109852ed7387c31390ff069554c0cc3d0a86c
shrek301 <tim_lat@mail.ru>
2018-12-17 11:54:34 +0000

    new code for EmployeeService

--- src/main/java/ru/latypov/service/EmployeeService.java
+++ src/main/java/ru/latypov/service/EmployeeService.java
@@ -1,9 +1,19 @@
 package ru.latypov.service;
 
-import org.springframework.stereotype.Service;
 
-@Service
-public class EmployeeService {
+import org.springframework.http.ResponseEntity;
+import ru.latypov.model.Employee;
 
+import java.util.List;
+/*Набор методов для контролера Работник */
+public interface EmployeeService {
+    public List<Employee> retrieveEmployees();
 
+    public Employee getEmployee(Integer id);
+
+    public void saveEmployee(Employee employee);
+
+    public ResponseEntity updateEmployee(Employee employee);
+
+    public Employee getEmployee(Employee employee);
 }
