e62c94df2c1763613c5ca597c347f94775f597b8
shrek301 <tim_lat@mail.ru>
2018-12-18 12:16:29 +0000

    добавлен код

--- src/main/java/ru/latypov/repository/OrganizationRepository.java
+++ src/main/java/ru/latypov/repository/OrganizationRepository.java
@@ -1,4 +1,8 @@
 package ru.latypov.repository;
 
-public class OrganizationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.latypov.model.Organization;
+@Repository
+public interface OrganizationRepository extends JpaRepository <Organization, Integer> {
 }
