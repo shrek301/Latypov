95fac869da52aa494eacc14d88e3c6853cf0b020
shrek301 <tim_lat@mail.ru>
2018-12-18 12:15:16 +0000

    добавлен код

--- src/main/java/ru/latypov/repository/OfficeRepository.java
+++ src/main/java/ru/latypov/repository/OfficeRepository.java
@@ -1,4 +1,8 @@
 package ru.latypov.repository;
 
-public class OfficeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.latypov.model.Office;
+@Repository
+public interface OfficeRepository extends JpaRepository <Office, Integer> {
 }
