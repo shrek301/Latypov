4ca2335056a52c1c07029a6b27774b95e7e69083
shrek301 <tim_lat@mail.ru>
2018-12-18 12:14:05 +0000

    добавлен код

--- src/main/java/ru/latypov/repository/EmployeeRepository.java
+++ src/main/java/ru/latypov/repository/EmployeeRepository.java
@@ -1,7 +1,8 @@
 package ru.latypov.repository;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.latypov.model.Employee;
+@Repository
+public interface   EmployeeRepository  extends JpaRepository <Employee, Integer> {
 }
