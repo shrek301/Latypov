5f76fb48b51a073f819d919535c9c88e89203c1a
shrek301 <tim_lat@mail.ru>
2018-12-18 12:18:24 +0000

    новый код

--- src/main/java/ru/latypov/service/Imp/OfficeServiceImpl.java
+++ src/main/java/ru/latypov/service/Imp/OfficeServiceImpl.java
@@ -1,19 +1,19 @@
 package ru.latypov.service.Imp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import ru.latypov.model.Office;
 import ru.latypov.repository.OfficeRepository;
 import ru.latypov.service.OfficeService;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class OfficeServiceImpl implements OfficeService {
     @Autowired
     private OfficeRepository officeRepository;
     public List<Office> retrieveOffice(){
-        List <Office> office=officeRepository.findALL();
+        List <Office> office=officeRepository.findAll();
         return office;
     }
     public Office getOffice (Integer id) {
@@ -21,10 +21,10 @@ public class OfficeServiceImpl implements OfficeService {
         return optEmp.get();
     }
     public Office  getOffice (Office office){
-        Optional<Office> optEmp1=officeRepository.findByemployee(office);
+        Optional<Office> optEmp1=officeRepository.findById(office.getId());
         return optEmp1.get();
     }
-    public ResponseEntity updateOffice(Office office){
+    public Office updateOffice(Office office){
         return officeRepository.save(office);
     }
     public void savesOffice (Office office){
