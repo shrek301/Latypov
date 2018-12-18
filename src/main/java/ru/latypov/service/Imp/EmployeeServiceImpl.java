45817eb236a8dcad488c26cdd1eb49e6a623b254
shrek301 <tim_lat@mail.ru>
2018-12-18 12:17:39 +0000

    добавлен код

--- src/main/java/ru/latypov/service/Imp/EmployeeServiceImpl.java
+++ src/main/java/ru/latypov/service/Imp/EmployeeServiceImpl.java
@@ -1,35 +1,41 @@
 package ru.latypov.service.Imp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.latypov.repository.EmployeeRepository;
 import ru.latypov.model.Employee;
 import ru.latypov.service.EmployeeService;
 
+
 import java.util.List;
 import java.util.Optional;
+
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
-    public List<Employee> retrieveEmployees(){
-        List <Employee> employees=employeeRepository.findALL();
+
+    public List<Employee> retrieveEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
         return employees;
     }
-    public Employee getEmployee (Integer id) {
+
+    public Employee getEmployee(Integer id) {
         Optional<Employee> optEmp = employeeRepository.findById(id);
         return optEmp.get();
     }
-    public Employee  getEmployee (Employee employee){
-        Optional<Employee> optEmp1=employeeRepository.findByemployee(employee);
+
+    public Employee getEmployee(Employee employee) {
+        Optional<Employee> optEmp1 = employeeRepository.findById(employee.getId());
         return optEmp1.get();
     }
-    public ResponseEntity updateEmployee(Employee employee){
+
+    public Employee updateEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
-    public void saveEmployee (Employee employee){
+
+    public void saveEmployee(Employee employee) {
         employeeRepository.save(employee);
     }
 }
